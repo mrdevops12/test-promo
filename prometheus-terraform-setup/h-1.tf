9696e3e024d394e256530979afbca835c3f70522
mrdevops12 <67409507+mrdevops12@users.noreply.github.com>
2025-06-19 21:44:00 +0530

    Update h-1.tf

--- prometheus-terraform-setup/h-1.tf
+++ prometheus-terraform-setup/h-1.tf
@@ -2,27 +2,45 @@ resource "helm_release" "demo_prometheus_stack" {
   name             = "demo-prometheus"
   namespace        = "demo-monitoring"
   create_namespace = true
+  chart            = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  version          = "56.6.2"
 
-  chart       = "kube-prometheus-stack"
-  repository  = "https://prometheus-community.github.io/helm-charts"
-  version     = "56.6.2"
-
-  # <-- skip all CRDs at the Terraform level -->
   skip_crds = true
 
-  # <-- disable the chart’s internal CRD hooks -->
-  set { name = "installCRDs"                    value = "false" }
-  set { name = "crds.enabled"                   value = "false" }
-  set { name = "prometheusOperator.crdInstall"  value = "false" }
+  # ── disable the chart’s own CRD hooks ──
+  set {
+    name  = "installCRDs"
+    value = "false"
+  }
+  set {
+    name  = "prometheusOperator.crdInstall"
+    value = "false"
+  }
 
-  # <-- disable recreating any cluster-wide RBAC -->
-  set { name = "global.rbac.create"                 value = "false" }
-  set { name = "prometheusOperator.createCustomResource" value = "false" }
+  # ── skip shared RBAC & CR creations ──
+  set {
+    name  = "global.rbac.create"
+    value = "false"
+  }
+  set {
+    name  = "prometheusOperator.createCustomResource"
+    value = "false"
+  }
 
-  # <-- your usual UI & LB settings -->
-  set { name = "prometheus.service.type"          value = "LoadBalancer" }
-  set { name = "grafana.enabled"                 value = "true" }
-  set { name = "grafana.service.type"            value = "LoadBalancer" }
+  # ── enable external access ──
+  set {
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
 
-  timeout = 600  # allow 10 min for AWS ELB provisioning
+  timeout = 600
 }
