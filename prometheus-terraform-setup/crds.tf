c480970c0e1999280c2d4a1eda1819cad7714893
mrdevops12 <67409507+mrdevops12@users.noreply.github.com>
2025-06-20 18:35:22 +0530

    Update crds.tf

--- prometheus-terraform-setup/crds.tf
+++ prometheus-terraform-setup/crds.tf
@@ -1,17 +1,29 @@
 resource "null_resource" "install_prom_crds" {
-  # whenever you bump chart version here, re‐run CRD install
   triggers = {
     chart_version = "56.6.2"
   }
 
   provisioner "local-exec" {
-    command = <<EOT
-      set -e
-      helm pull prometheus-community/kube-prometheus-stack \
-        --version ${self.triggers.chart_version} --untar --untardir ./crd_tmp
-      kubectl apply -f crd_tmp/kube-prometheus-stack/crds/
-      rm -rf crd_tmp
-    EOT
     interpreter = ["bash", "-c"]
+    command     = <<-EOT
+      set -e
+
+      # 1) Ensure the Prometheus Community repo is added
+      if ! helm repo list | grep -q '^prometheus-community'; then
+        helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+      fi
+      helm repo update
+
+      # 2) Pull & unpack the exact chart version
+      helm pull prometheus-community/kube-prometheus-stack \
+        --version ${self.triggers.chart_version} \
+        --untar --untardir ./crd_tmp
+
+      # 3) Apply its CRDs
+      kubectl apply -f ./crd_tmp/kube-prometheus-stack/crds/
+
+      # 4) Clean up
+      rm -rf ./crd_tmp
+    EOT
   }
 }
