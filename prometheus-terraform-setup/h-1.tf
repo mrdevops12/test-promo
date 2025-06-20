ffc83c8879bb3207382f80b654b6e34ee51feed0
mrdevops12 <67409507+mrdevops12@users.noreply.github.com>
2025-06-20 18:02:32 +0530

    Update h-1.tf

--- prometheus-terraform-setup/h-1.tf
+++ prometheus-terraform-setup/h-1.tf
@@ -1,47 +1,37 @@
-resource "helm_release" "demo_prometheus_stack" {
-  name             = "demo-prometheus"
-  namespace        = "demo-monitoring"
-  create_namespace = true
-  chart            = "kube-prometheus-stack"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  version          = "56.6.2"
+ resource "helm_release" "demo_prometheus_stack" {
+   name             = "demo-prometheus"
+   namespace        = "demo-monitoring"
+   create_namespace = true
+   chart            = "kube-prometheus-stack"
+   repository       = "https://prometheus-community.github.io/helm-charts"
+   version          = "56.6.2"
 
-  skip_crds = true
+   skip_crds = true
 
-  # ── disable the chart’s own CRD hooks ──
-  set {
-    name  = "installCRDs"
-    value = "false"
-  }
-  set {
-    name  = "prometheusOperator.crdInstall"
-    value = "false"
-  }
++  depends_on = [
++    null_resource.install_prom_crds
++  ]
 
-  # ── skip shared RBAC & CR creations ──
-  set {
-    name  = "global.rbac.create"
-    value = "false"
-  }
-  set {
-    name  = "prometheusOperator.createCustomResource"
-    value = "false"
-  }
+   set {
+     name  = "global.rbac.create"
+     value = "false"
+   }
+   set {
+     name  = "prometheusOperator.createCustomResource"
+     value = "false"
+   }
+   set {
+     name  = "prometheus.service.type"
+     value = "LoadBalancer"
+   }
+   set {
+     name  = "grafana.enabled"
+     value = "true"
+   }
+   set {
+     name  = "grafana.service.type"
+     value = "LoadBalancer"
+   }
 
-  # ── enable external access ──
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "grafana.enabled"
-    value = "true"
-  }
-  set {
-    name  = "grafana.service.type"
-    value = "LoadBalancer"
-  }
-
-  timeout = 600
-}
-
+   timeout = 600
+ }
