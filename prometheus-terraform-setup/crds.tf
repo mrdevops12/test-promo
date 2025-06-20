e8ade1c4f4881e3660e17f508a39ba7cde0887ee
mrdevops12 <67409507+mrdevops12@users.noreply.github.com>
2025-06-20 18:30:45 +0530

    Update crds.tf

--- prometheus-terraform-setup/crds.tf
+++ prometheus-terraform-setup/crds.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "install_prom_crds" {
-  # Any change to the CRD pack (or chart version) will re-run this
+  # whenever you bump chart version here, re‐run CRD install
   triggers = {
     chart_version = "56.6.2"
   }
@@ -7,12 +7,10 @@ resource "null_resource" "install_prom_crds" {
   provisioner "local-exec" {
     command = <<EOT
       set -e
-      # download & unpack the chart
       helm pull prometheus-community/kube-prometheus-stack \
-        --version ${self.triggers.chart_version} --untar --untardir ./crds_temp
-      # apply just the CRDs
-      kubectl apply -f ./crds_temp/kube-prometheus-stack/crds/
-      rm -rf ./crds_temp
+        --version ${self.triggers.chart_version} --untar --untardir ./crd_tmp
+      kubectl apply -f crd_tmp/kube-prometheus-stack/crds/
+      rm -rf crd_tmp
     EOT
     interpreter = ["bash", "-c"]
   }
