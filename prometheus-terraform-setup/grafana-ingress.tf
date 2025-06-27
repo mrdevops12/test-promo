c5033a117479f77a4a3566269d56971a112c5cad
mrdevops12 <67409507+mrdevops12@users.noreply.github.com>
2025-06-27 15:04:12 +0530

    Update grafana-ingress.tf

--- prometheus-terraform-setup/grafana-ingress.tf
+++ prometheus-terraform-setup/grafana-ingress.tf
@@ -1,14 +1,14 @@
 resource "kubernetes_ingress_v1" "grafana_ingress" {
   metadata {
-    name      = "grafana"
+    name      = "grafana-ingress"
     namespace = "demo-monitoring"
 
     annotations = {
-      kubernetes.io/ingress.class                     = "alb"
-      alb.ingress.kubernetes.io/scheme                = "internal"
-      alb.ingress.kubernetes.io/target-type           = "ip"
-      alb.ingress.kubernetes.io/listen-ports          = "[{\"HTTP\":80}]"
-      alb.ingress.kubernetes.io/load-balancer-attributes = "idle_timeout.timeout_seconds=60"
+      "kubernetes.io/ingress.class"                             = "alb"
+      "alb.ingress.kubernetes.io/scheme"                        = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"                   = "ip"
+      "alb.ingress.kubernetes.io/listen-ports"                  = "[{\"HTTP\":80}]"
+      "alb.ingress.kubernetes.io/load-balancer-attributes"      = "idle_timeout.timeout_seconds=60"
     }
   }
 
@@ -16,8 +16,8 @@ resource "kubernetes_ingress_v1" "grafana_ingress" {
     rule {
       http {
         path {
-          path     = "/*"
-          path_type = "ImplementationSpecific"
+          path      = "/*"
+          path_type = "Prefix"
 
           backend {
             service {
@@ -31,6 +31,4 @@ resource "kubernetes_ingress_v1" "grafana_ingress" {
       }
     }
   }
-
-  depends_on = [helm_release.prometheus_stack]
 }
